148e65aa69bfb37ab0a4979bd0116422f1f90339
李德荣 <773836629@qq.com>
2023-02-03 17:40:58 +0800

    面包屑修改

--- src/components/Hamburger/Hamburger.vue
+++ src/components/Hamburger/Hamburger.vue
@@ -6,13 +6,15 @@
       :class="[{'is-active': isActive}]"
       @click="toggleClick"
     >
-      <svg
+      <!-- <svg
         class="icon"
         aria-hidden="true"
         font-size="20px"
       >
         <use xlink:href="#iconhamburger" />
-      </svg>
+      </svg> -->
+      <!-- <svg viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" data-v-029747aa=""><path fill="currentColor" d="M896 192H128v128h768V192zm0 256H384v128h512V448zm0 256H128v128h768V704zM320 384 128 512l192 128V384z"></path></svg> -->
+      <el-icon><Fold /></el-icon>
     </div>
   </template>
   
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,15 @@ import App from './App.vue'
 import Router from './router/router'
 import { axiosPlugin } from './utils/axios'
 import { createPinia } from "pinia";
+// 图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 const pinia = createPinia();
 const app = createApp(App)
+// 注册所有图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 app.use(Router)
     .use(ElementPlus)
     .use(axiosPlugin)
